42c5ab72ad021cc19367bd8d467c36bbcc69981a
George <zhora.sviridov.05@bk.ru>
2024-09-24 23:33:51 +0500

    first commit

--- app/views.py
+++ app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    return HttpResponse("<h4>Проверка работы</h4>")
+
+def login(request):
+    return  HttpResponse("<h4>Зарегестрироваться</h4>")
