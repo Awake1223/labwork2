2670adc49b6316261dc3b8afd22ad3646469f075
George <zhora.sviridov.05@bk.ru>
2024-09-29 18:56:57 +0500

    the last commit

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from .views import set_cookie, get_cookie
 
 urlpatterns = [
     path('',views.index, name='home' ),
-    path('login',views.login, name='login')
+    path('login',views.login, name='login'),
+    path ('payment', views.payment, name='payment'),
+    path('set_cookie/', set_cookie, name='set_cookie'),
+    path('get_cookie/', get_cookie, name='get_cookie'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,6 @@
+from http.client import responses
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -7,3 +10,28 @@ def index(request):
 
 def login(request):
     return  render(request, 'app/login.html')
+
+def payment(request):
+   return  render(request, 'app/payment.html')
+
+
+def set_cookie(request):
+    login = request.GET.get('login')
+    password = request.GET.get('password')
+    user_value = request.GET.get('value', None)
+
+    # Устанавливаем куки
+    response = HttpResponse("Кука установлена!")
+    response.set_cookie('login', login)  # Сохраняем логин в куки
+    response.set_cookie('password', password)  # Сохраняем пароль в куки (обычно не рекомендуется)
+    response.set_cookie('user_value', user_value)
+    return response
+
+
+def get_cookie(request):
+    login = request.COOKIES.get('login', 'Нет логина.')
+    password = request.COOKIES.get('password', 'Нет пароля.')
+    user_value = request.COOKIES.get('user_value', 'Кука не установлена.')
+
+    # Отображаем значения куки на сайте
+    return render(request, 'login.html', {'login': login, 'password': password})
